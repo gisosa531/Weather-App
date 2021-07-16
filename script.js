0e5fe68d0d84d6e2f8e92be46804bcf2e5b882ab
gisosa <gio53196@gmail.com>
2021-07-16 09:49:29 -0700

    Initial Skeleton text for JS and HTML

---
+++ script.js
@@ -0,0 +1,19 @@
+var APIKey = "e6bc155639e8522b14cb4091f92c33e5";
+
+
+function getWeather(event){
+    var inputEl=document.getElementById("cityInput");
+    var cityName= inputEl.value;
+    var apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`
+
+
+    fetch(apiLink).then(function(res){
+        return res.json();
+    }) .then(function(res){
+        console.log(res);
+    })
+}
+
+var submitEl=document.getElementById("submitBtn");
+submitEl.addEventListener("click", getWeather);
+
